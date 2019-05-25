2ea0ac04dcae74f41c351c45730ae3d36387e893
shantoislam6 <ishanto412@gmail.com>
2019-05-25 22:35:56 +0600

    All commited together

---
+++ index.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const path    = require('path');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+const exphbs = require('express-handlebars');
+const app = express();
+
+// View Engine Setup
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+// set static folder
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+// set body parser
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
+// App Router
+app.get('/', (req, res) => {
+    res.render('contact');
+});
+
+app.post('/send',(req, res)=>{
+    const content = `
+    <p>You Have a new contact request</p>
+    <h3>Contact Details</h3>
+    <ul>
+        <li style="color:red">${ req.body.name }</li>
+        <li style="color:blue">${ req.body.company }</li>
+        <li style="color:green">${ req.body.email }</li>
+        <li style="color:orange">${ req.body.phone }</li>
+    </ul>
+    <h3>Message</h3>
+    <p>${ req.body.message }</p>
+ `;
+
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+        host: "mail.developermaruf.com",
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: 'shanto@developermaruf.com', // generated ethereal user
+            pass: 'yQyRma7hAOa_' // generated ethereal password
+        },
+        tls:{
+            rejectUnauthorized:false
+        }
+    });
+
+    // send mail with defined transport object
+    let info = transporter.sendMail({
+        from: '"NodeMailer Contact" <shanto@developermaruf.com>', // sender address
+        to: req.body.email, // list of receivers
+        subject: "Node Contact Request", // Subject line
+        text: "Hello world?", // plain text body
+        html: content // html body
+    });
+    res.render('contact', {msg: 'The contact has been sent'});
+});
+
+//create a server
+app.listen(3000,() => console.log('Listening on http://localhost:3000'));
